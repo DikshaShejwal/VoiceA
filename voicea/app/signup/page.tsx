cdaf6e60065598b9cddd27ac848bc09ee4b075e1
DikshaShejwal <168258337+DikshaShejwal@users.noreply.github.com>
2025-04-06 14:28:35 +0530

    Update page.tsx

--- voicea/app/signup/page.tsx
+++ voicea/app/signup/page.tsx
@@ -58,7 +58,7 @@ export default function SignUpPage() {
     };
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/signup", {
+      const response = await fetch("https://voicea-ny1b.onrender.com/api/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(newUser),
