0e4617acb3c3c7e5b59abfee60ed98e4290afebf
DikshaShejwal <168258337+DikshaShejwal@users.noreply.github.com>
2025-04-06 15:28:43 +0530

    Update global.d.ts

--- voicea/app/global.d.ts
+++ voicea/app/global.d.ts
@@ -6,9 +6,26 @@ declare global {
     webkitSpeechRecognition: typeof SpeechRecognition;
   }
 
-  var SpeechRecognition: typeof SpeechRecognition;
+  var SpeechRecognition: typeof window.webkitSpeechRecognition;
+
+  interface SpeechRecognitionResult {
+    isFinal: boolean;
+    length: number;
+    [index: number]: SpeechRecognitionAlternative;
+  }
+
+  interface SpeechRecognitionAlternative {
+    transcript: string;
+    confidence: number;
+  }
+
+  interface SpeechRecognitionResultList {
+    length: number;
+    [index: number]: SpeechRecognitionResult;
+  }
 
   interface SpeechRecognitionEvent extends Event {
+    readonly resultIndex: number;
     readonly results: SpeechRecognitionResultList;
   }
 }
